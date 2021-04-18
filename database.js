f8a56c7a5142b9918a118b3c934bc1339b66112f
Shawn Lee <eesy96@gmail.com>
2021-04-17 21:42:57 -0600

    add db comments

--- database.js
+++ database.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
-
-// move these to env lol
-// blowfAdmin
-// rv3Fk1dsrZZw5w60 
-
+// production: blowf-nfts.nuccx.mongodb.net
 const connection = "mongodb+srv://blowfAdmin:rv3Fk1dsrZZw5w60@blowf-cluster.yp2un.mongodb.net/blowf?retryWrites=true&w=majority";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
