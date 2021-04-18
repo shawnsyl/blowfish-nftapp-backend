4dcf4314478486217c1c89ac6b738eb8bddfd648
Shawn Lee <eesy96@gmail.com>
2021-04-17 21:46:37 -0600

    remove db password

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 // production: blowf-nfts.nuccx.mongodb.net
-const connection = "mongodb+srv://blowfAdmin:rv3Fk1dsrZZw5w60@blowf-cluster.yp2un.mongodb.net/blowf?retryWrites=true&w=majority";
+const connection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@blowf-cluster.yp2un.mongodb.net/blowf?retryWrites=true&w=majority`
+
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
