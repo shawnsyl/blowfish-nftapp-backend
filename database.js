2578214a437bc2829cf4cd06971839ccfbafcae0
Shawn Lee <eesy96@gmail.com>
2021-04-17 21:49:22 -0600

    add dotenv config

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname+'/.env')});
 // production: blowf-nfts.nuccx.mongodb.net
+
 const connection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@blowf-cluster.yp2un.mongodb.net/blowf?retryWrites=true&w=majority`
 
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
