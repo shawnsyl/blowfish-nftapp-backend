8397becbc2d8bf9780e915bfc105bc90f806501d
Shawn Lee <eesy96@gmail.com>
2021-04-20 15:53:45 -0600

    add production mongodb url

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname+'/.env')});
-// production: blowf-nfts.nuccx.mongodb.net
+// production: blowf-nfts.nuccx.mongodb.net 
+// staging: blowf-cluster.yp2un.mongodb.net
 
-const connection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@blowf-cluster.yp2un.mongodb.net/blowf?retryWrites=true&w=majority`
+const dbUri = process.env.IS_STAGING === 'TRUE' ? 'blowf-cluster.yp2un.mongodb.net' : 'blowf-nfts.nuccx.mongodb.net'
+
+const connection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${dbUri}/blowf?retryWrites=true&w=majority`
 
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
