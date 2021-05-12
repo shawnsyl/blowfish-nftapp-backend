1e677cba27275f9d47779abfe0d0297061f46324
Shawn Lee <eesy96@gmail.com>
2021-05-11 20:27:16 -0600

    use env vars for db uri

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: path.resolve(__dirname+'/.env')});
 // production: blowf-nfts.nuccx.mongodb.net 
 // staging: blowf-cluster.yp2un.mongodb.net
 
-const dbUri = process.env.NODE_ENV == 'develop' || process.env.IS_STAGING === 'TRUE' ? 'blowf-cluster.yp2un.mongodb.net' : 'blowf-nfts.nuccx.mongodb.net'
+const dbUri = process.env.NODE_ENV == 'develop' || process.env.IS_STAGING === 'TRUE' ? process.env.DB_URI_TEST : process.env.DB_URI_PROD
 
 const connection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${dbUri}/blowf?retryWrites=true&w=majority`
 
